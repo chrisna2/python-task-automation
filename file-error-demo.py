f8898369ba9e5aacdbeea6832a88591339a1c909
chrisna2 <chrisna2@hanmail.net>
2019-08-21 11:24:04 +0900

    업무자동화 : 2일차

---
+++ file-error-demo.py
@@ -0,0 +1,16 @@
+try:
+    # 에러가 발생 할 수 있는 코드
+    with open("hell.txt","r",encoding='utf-8') as file:
+        lines = file.readlines()
+        print(lines)
+
+except:
+    # 에러 대신에 발생하는 코드
+    print("파일을 읽을 수 없습니다.")
+
+
+print("파일 읽기 종료.")
+
+# 에러가 발생하면 코드가 죽어 버린다.
+# 따라서 에러 처리를 해야 한다.
+
--- file-with-context.py
+++ file-with-context.py
@@ -2,7 +2,7 @@
 #lines = file.readlines()
 #print(lines)
 
-
+# with 파일이 자동으로 닫힌다. file.close 자동실행
 with open("hello.txt","r",encoding='utf-8') as file:
     lines = file.readlines()
     print(lines)
